25fbca23e9b3cd208fb6a90a5a90bbf7e96ed894
paulcosma <paulcosma@gmail.com>
2016-04-04 02:36:15 +0300

    add debug

--- calculator_spec.js
+++ calculator_spec.js
@@ -5,6 +5,7 @@ describe ('test the calculator demo site', function () {
         element(by.model("first")).sendKeys("1");
         element(by.model("second")).sendKeys("2");
         browser.pause(); //Enter c to move the test forward by one task. Enter repl to enter interactive mode,  exit by pressing Ctrl-C
+        //browser.debugger(); run test in debug mode with protractor debug conf.js
         element(by.id("gobutton")).click();
         var resultBinding  = element(by.binding('latest'));
         //  element(resultBinding).getText().then(function (text) {
