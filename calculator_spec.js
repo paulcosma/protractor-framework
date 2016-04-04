b7bb3bafd3537a46169d13b0536cc2fe15a728dd
paulcosma <paulcosma@gmail.com>
2016-04-04 17:52:49 +0300

    log the text of element

--- calculator_spec.js
+++ calculator_spec.js
@@ -4,13 +4,14 @@ describe ('test the calculator demo site', function () {
 
         element(by.model("first")).sendKeys("1");
         element(by.model("second")).sendKeys("2");
-        //browser.pause(); //Enter c to move the test forward by one task. Enter repl to enter interactive mode,  exit by pressing Ctrl-C
-        //browser.debugger(); run test in debug mode with protractor debug conf.js
+        //browser.pause(); // Enter c to move the test forward by one task. Enter repl to enter interactive mode,  exit by pressing Ctrl-C
+        //browser.debugger(); // run test in debug mode with protractor debug conf.js
         element(by.id("gobutton")).click();
+        // log the text of an element
         var resultBinding  = element(by.binding('latest'));
-        //  element(resultBinding).getText().then(function (text) {
-        //     console.log(text);
-        // });
+        resultBinding.getText().then(function(text) {
+            console.log("Result:= " + text);
+        });
         expect(resultBinding.getText()).toEqual('3');
     });
 });
