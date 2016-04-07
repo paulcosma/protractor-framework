e115923892adcee01906ce004631eaf396de583c
paulcosma <paulcosma@gmail.com>
2016-04-07 13:26:18 +0300

    add comments

--- src/page/CalculatorResult.js
+++ src/page/CalculatorResult.js
@@ -6,11 +6,19 @@ var CalculatorResult = function(){
     //     element(by.model('name')).$('[value="' + index + '"]').click();
     // }
 
+    /**
+     *
+     * @returns {webdriver.promise.Promise<string>}
+     */
     this.getResultText = function(){
                 resultBinding.getText().then(function(text) {
             console.log("Result:= " + text);
         });
         return resultBinding.getText();
+        // OR
+        // var text = resultBinding.getText()
+        // console.log("Result:= " + text);
+        // return text;
     };
 
 };
