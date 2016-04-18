1577f4534edbc0fc2d1391f07d9ea8f6a3b21beb
cosmap <cosmap@media-saturn.com>
2016-04-18 11:29:41 +0300

    add JSDoc

--- src/page/CalculatorHome.js
+++ src/page/CalculatorHome.js
@@ -8,26 +8,41 @@ var CalculatorHome = function () {
     var pageName = element(by.xpath('//h3'));
 
     // Methods
-    // get Page
+    /**
+     * get Page
+     */
     this.get = function () {
         browser.get("http://juliemr.github.io/protractor-demo/");
     };
 
-    // set Value for first field
+    /**
+     * set Value for first field
+     * @param value for the first field
+     */
     this.enterFirstFieldValue = function (value) {
         firstField.sendKeys(value);
     };
 
-    // set Value for Second field
+    /**
+     * set Value for Second field
+     * @param value for the second field
+     */
     this.enterSecondFieldValue = function (value) {
         secondField.sendKeys(value);
     };
 
+    /**
+     * get then Page Name displayed on top of the page
+     * @returns Page Name text{webdriver.promise.Promise<string[]>|webdriver.promise.Promise<string>}
+     */
     this.getPageName = function () {
         return pageName.getText();
     };
 
-    // click on Go button
+    /**
+     * click on Go button
+     * @returns {CalculatorResult}
+     */
     this.clickGoButton = function () {
         goButton.click();
         return require('../page/CalculatorResult.js'); // this must be imported as well
