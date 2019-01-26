559f90e5f6148fa0c0a9d0c99080f6a7e1d91fae
Paul Cosma <paulcosma@gmail.com>
2019-01-26 15:57:21 +0200

    fix environment

--- src/com/paulcosma/calculator/conf/environment.js
+++ src/com/paulcosma/calculator/conf/environment.js
@@ -8,10 +8,9 @@
  */
 var ENV = process.env.NODE_ENV || '';
 
-// Application: "idp", "pico", "ifora"
+// Application: "calculator", "angular"
 var APP = "calculator";
 
-// NPM node_modules path: Ubuntu14 = '/usr/local/lib/node_modules/', '/usr/lib/node_modules/', Windows: 'C:/Users/<user>/AppData/Roaming/npm/node_modules/'
 // or local, on project folder '../../../../../node_modules/'
 var npmNodeModulesPath = '../../../../../node_modules/';
 
@@ -21,6 +20,5 @@ module.exports = {
     // Application full URL
     URL: "https://" + APP + ENV + ".paulcosma.com",
 
-    // NPM path is different on Windows VM and Linux VM
     NPM_NODE_MODULES_PATH: npmNodeModulesPath,
 };
