d439e0d9f1fb1d69294884ecaf512ffa6a951c5b
Paul Cosma <paulcosma@gmail.com>
2019-01-31 21:11:33 +0200

    cleanup

--- src/com/paulcosma/calculator/test/asyncawait_xspec.js
+++
@@ -1,8 +0,0 @@
-/* global protractor,describe,require,it,expect,element,by,browser */
-var env = require('../conf/environment.js');
-xdescribe('Async Await suite', function() {
-    it('load from valid link', async function() {
-        await browser.get(env.URL);
-        await expect(browser.getCurrentUrl()).toEqual('https://calculator.paulcosma.com/');
-    });
-});
