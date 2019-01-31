ff926ce952e17b26fb3250d58fb7f903d422cd4a
Paul Cosma <paulcosma@gmail.com>
2019-01-31 21:22:51 +0200

    get console errors after each test
    remove focus

--- src/com/paulcosma/calculator/test/openApp_spec.js
+++ src/com/paulcosma/calculator/test/openApp_spec.js
@@ -5,6 +5,27 @@ describe('Protractor Calculator Demo App', function() {
         return window.angular.version.full;
     }
 
+    function getBrowserConsoleErrors() {
+        var deferred = protractor.promise.defer(); // promise
+        browser.manage().logs().get('browser').then(function (browserLogs) {
+            // browserLogs is an array of objects with level and message fields
+            var result = '';
+            browserLogs.forEach(function (log) {
+                if (log.level.value > 900) { // it's an error log
+                    console.log('\n Browser console errors = ' + log.message);
+                    console.log('\n Browser console log = ' + require('util').inspect(log));
+                    result += JSON.stringify(require('util').inspect(log));
+                }
+            });
+            deferred.fulfill(result);
+        });
+        return deferred.promise;
+    }
+
+    afterEach(function () {
+        getBrowserConsoleErrors();
+    });
+
     it('should load from valid link', function() {
         browser.get('https://calculator.paulcosma.com/');
         // browser.pause();
@@ -13,6 +34,5 @@ describe('Protractor Calculator Demo App', function() {
         });
         expect(browser.getCurrentUrl()).toEqual('https://calculator.paulcosma.com/');
     });
-
 
 });
