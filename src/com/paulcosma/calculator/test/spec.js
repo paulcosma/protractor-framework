de49c5b11f19df13f65042fe55a882a68310eb28
Paul Cosma <paulcosma@gmail.com>
2019-02-08 21:32:35 +0200

    queue pending promises and control flow example

--- src/com/paulcosma/calculator/test/spec.js
+++ src/com/paulcosma/calculator/test/spec.js
@@ -1,7 +1,12 @@
 /* global protractor,describe,require,it,expect,element,by,browser */
 describe('Protractor Demo App', function() {
     it('should load from valid link', function() {
-        browser.get('https://calculator.paulcosma.com/');
+        console.log("This will be printed 1st");
+        browser.get('https://calculator.paulcosma.com/').then(function(){
+            console.log("This will be printed 3rd");
+            }
+        );
         expect(browser.getCurrentUrl()).toEqual('https://calculator.paulcosma.com/');
+        console.log("This will be printed 2nd");
     });
 });
