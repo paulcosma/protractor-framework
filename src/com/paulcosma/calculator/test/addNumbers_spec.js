b421d6a57afdf1103dff547acf3dc0000e6fb978
Cosma, Paul (Accesa) <cosmap@mediamarktsaturn.com>
2019-04-23 16:31:29 +0300

    console log and promise example
    log line no in console

--- src/com/paulcosma/calculator/test/addNumbers_spec.js
+++ src/com/paulcosma/calculator/test/addNumbers_spec.js
@@ -10,15 +10,25 @@ describe('Calculator Operations', function() {
 
     testParams.forEach( function(testSpec) {
         it('add numbers test', async function() {
-            console.log("Debug: a = " + testSpec.a);
-            console.log("Debug: b = " + testSpec.b);
-            console.log("Debug: result = " + testSpec.result);
+            console.log("Debug: line 13 a = " + testSpec.a);
+            console.log("Debug: line 14 b = " + testSpec.b);
+            console.log("Debug: line 15 result = " + testSpec.result);
             await browser.get(env.URL);
+            console.log("Debug: line 17");
             await element(by.model("first")).sendKeys(testSpec.a);
+            console.log("Debug: line 19");
             await element(by.model("second")).sendKeys(testSpec.b);
             await element(by.id("gobutton")).click();
-            console.log("Debug: actual result = " + await element(by.css('.ng-binding')).getText());
-            expect(await element(by.css('.ng-binding')).getText()).toEqual(testSpec.result.toString());
+            console.log("Debug: line 22 actual result = " + await element(by.css('h2.ng-binding')).getText());
+            // previous console log using promises
+            element(by.css('h2.ng-binding')).getText().then(function (value) {
+                console.log("Debug: line 25 actual result promise = " + value);
+            });
+            expect(await element(by.css('h2.ng-binding')).getText()).toEqual(testSpec.result.toString());
+            console.log("Debug: line 28");
+            console.log("Debug line 29: a = " + testSpec.a);
+            console.log("Debug line 30: b = " + testSpec.b);
+            console.log("Debug line 31: result = " + testSpec.result);
         });
     });
 
